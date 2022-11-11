1acb9c1a0d7e4d1afcd006dafe3182324ba08f5a
shadrajput <shadrajput6@gmail.com>
2022-11-11 10:23:41 +0530

    ok

--- src/Componant/loader.js
+++ src/Componant/loader.js
@@ -1,15 +1,8 @@
-import React from "react";
-import {AiOutlineLoading} from "react-icons/ai"
-
-
-
-
-export default function Loader() {
+const Loader = () => {
     return <div className="w-full h-[90vh] flex justify-center items-center">
-    <div style={{borderTopColor:"transparent"}} className="mr-2 w-10 h-10 border-4 border-blue-400 border-solid rounded-full animate-spin"></div>
-    <h2 className="text-2xl text-gray-700">Loading...</h2>
-</div>
+                <div style={{borderTopColor:"transparent"}} className="mr-2 w-10 h-10 border-4 border-blue-400 border-solid rounded-full animate-spin"></div>
+                <h2 className="text-2xl text-gray-700">Loading...</h2>
+            </div>
 }
 
-
-
+export default Loader

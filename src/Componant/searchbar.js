421a621ce17811f8cfdf953db05e9849b1a4a5dd
moinuddin2021 <moinuddinchudiwal2021@gmail.com>
2022-12-03 08:45:55 -0800

    Searchba toggle icon solve and onclick on profile solved

--- src/Componant/searchbar.js
+++ src/Componant/searchbar.js
@@ -30,21 +30,19 @@ export default function Searchbar() {
     changeSection();
   }
   return (
-    <div
-      onClick={handleToggle}
-      className="w-full z-[100] sticky top-0 bg-white h-[70px] flex flex-row items-center justify-between shadow-[0_10px_10px_-15px_rgba(0,0,0,0.3)]"
+    <div className="w-full z-[100] sticky top-0 bg-white h-[70px] flex flex-row items-center justify-between shadow-[0_10px_10px_-15px_rgba(0,0,0,0.3)]"
     >
       <div className="left pl-5 w-1/4">
         {/* This sectione empty for searchbar */}
       </div>
-      <div className="right">
+      <div className="right" onClick={handleToggle}>
         <div className="top grid grid-col-2 items-center cursor-pointer justify-right space-x-5 pr-5 static">
           <form
             action=""
             className="flex items-center space-x-2 cursor-pointer"
             id="profile"
           >
-            <div className="profile">
+            <div className="profile"> 
               <img
                 className="rounded-full w-10 mx-auto"
                 src="images/user.png"
@@ -79,8 +77,8 @@ export default function Searchbar() {
 
                 <NavLink to="/Componant/Updateprofile">
                   <div className="bg-white hover:bg-slate-200 text-gray-800  h-11 my-2 cursor-pointer hover:text-blue-500  flex justify-start px-2 hover:rounded-xl ml-4 mr-4 space-x-6  items-center">
-                    <div className="bg-blue-200 w-1/6 h-9 flex justify-center items-center rounded-full">
-                      <FaRegUserCircle className="text-blue-500  text-xl " />
+                    <div className="bg-blue-200 p-2.5 flex justify-center items-center rounded-full">
+                      <FaRegUserCircle className="text-blue-500 text-xl " />
                     </div>
                     <span className="md:text-sm xl:text-base">
                       Admin Profile
@@ -89,7 +87,7 @@ export default function Searchbar() {
                 </NavLink>
                 <NavLink className="nav-link" to="/Componant/Changepassword">
                   <div className="bg-white hover:bg-slate-200 text-gray-800  h-11 my-2 cursor-pointer hover:text-blue-500  flex justify-start px-2 hover:rounded-xl ml-4 mr-4 space-x-6  items-center">
-                    <div className="bg-blue-200  w-1/6 h-9 flex justify-center items-center rounded-full">
+                    <div className="bg-blue-200  p-2.5 flex justify-center items-center rounded-full">
                       <VscKey className="text-blue-500 text-xl" />
                     </div>
                     <span className="md:text-sm xl:text-base">
@@ -100,7 +98,7 @@ export default function Searchbar() {
 
                 <NavLink to="/Componant/Addadmin">
                   <div className="bg-white hover:bg-slate-200 text-gray-800  h-11 my-2 cursor-pointer hover:text-blue-500  flex justify-start px-2 hover:rounded-xl ml-4 mr-4 space-x-6  items-center">
-                    <div className="bg-blue-200  w-1/6 h-9 flex justify-center items-center rounded-full">
+                    <div className="bg-blue-200  p-2.5 flex justify-center items-center rounded-full">
                       <MdOutlinePersonAddAlt className="text-blue-500 text-xl" />
                     </div>
                     <span className="md:text-sm xl:text-base">Add Admin</span>
@@ -108,7 +106,7 @@ export default function Searchbar() {
                 </NavLink>
                 <NavLink to="/Componant/AdminList">
                   <div className="bg-white hover:bg-slate-200 text-gray-800  h-11 my-2 cursor-pointer hover:text-blue-500  flex justify-start px-2 hover:rounded-xl ml-4 mr-4 space-x-6  items-center">
-                    <div className="bg-blue-200  w-1/6 h-9 flex justify-center items-center rounded-full">
+                    <div className="bg-blue-200  p-2.5 flex justify-center items-center rounded-full">
                       <MdOutlinePersonAddAlt className="text-blue-500 text-xl" />
                     </div>
                     <span className="md:text-sm xl:text-base">Admin List</span>
@@ -116,7 +114,7 @@ export default function Searchbar() {
                 </NavLink>
                 <div className="nav-link" onClick={(e) => handleSection()}>
                   <div className="bg-white hover:bg-slate-200 text-gray-800  h-11 my-2 cursor-pointer hover:text-blue-500  flex justify-start px-2 hover:rounded-xl ml-4 mr-4 space-x-6  items-center">
-                    <div className="bg-blue-200 w-1/6 h-9  flex justify-center items-center rounded-full">
+                    <div className="bg-blue-200 p-2.5  flex justify-center items-center rounded-full">
                       <MdPublishedWithChanges className="text-blue-500 text-xl" />
                     </div>
                     <span className="md:text-sm xl:text-base">
@@ -130,7 +128,7 @@ export default function Searchbar() {
                   onClick={handleLogoutButton}
                   className="bg-white hover:bg-slate-200 text-gray-800  h-11 my-2 cursor-pointer hover:text-blue-500  flex justify-start px-2 hover:rounded-xl ml-4 mr-4 space-x-6  items-center"
                 >
-                  <div className="bg-blue-200  w-1/6 h-9 flex justify-center items-center rounded-full">
+                  <div className="bg-blue-200  p-2.5 flex justify-center items-center rounded-full">
                     <RiLogoutCircleRLine className="text-blue-500 text-xl" />
                   </div>
                   <span className="md:text-sm xl:text-base">Logout</span>
